b5e85d95a9eb7329005d5ffea6d56ecdb9158022
Your Name <eladyat@gmail.com>
2014-06-28 23:31:15 +0300

    add command and del the old

--- SimpleShell/src/asg/cliche/sample/HelloWorld.java
+++ SimpleShell/src/asg/cliche/sample/HelloWorld.java
@@ -6,15 +6,22 @@ import java.io.IOException;
 
 public class HelloWorld {
 
-    @Command // One,
+    @Command//the start command
     public String hello() {
         return "welcome to Galaxy Game";
     }
-    @Command // two,
-    public int add(int a, int b) {
-        return a + b;
+    @Command 
+    public void enter (String name) 
+    {
+    	
     }
-   
+    @Command 
+    public void list (String name)
+    {
+    	
+    }
+    
+    
 
    
     public static void main(String[] args) throws IOException {
