116416f9c4784e434bd952e50971630fbb507309
Your Name <eladyat@gmail.com>
2014-06-29 00:13:52 +0300

    login and register

--- SimpleShell/src/asg/cliche/sample/HelloWorld.java
+++ SimpleShell/src/asg/cliche/sample/HelloWorld.java
@@ -7,7 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
 public class HelloWorld {
+   private static String userName = null;
 
     @Command//the start command
     public String hello() {
@@ -23,28 +26,53 @@ public class HelloWorld {
     {
     	
     }
-    
+  private static  void Register()
+	{
+		
+	}
+	private static  boolean Login() 
+	{
+		boolean ret=false;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("username:");
+
+
+        try {
+           userName = br.readLine();
+        } catch (IOException ioe) {
+           System.out.println("IO error trying to read your name!");
+           System.exit(1);
+        }
+        
+
+   	System.out.println("password");
+   	return ret;
+	}
     public static void main(String[] args) throws IOException
     {
-    	System.out.println("Welcome to Galaxy");
-    	System.out.print("username:");
-    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-         String userName = null;
-
-         //  read the username from the command-line; need to use try/catch with the
-         //  readLine() method
-         try {
-            userName = br.readLine();
-         } catch (IOException ioe) {
-            System.out.println("IO error trying to read your name!");
-            System.exit(1);
-         }
-
-         System.out.println("Thanks for the name, " + userName);
-
-    	System.out.println("password");
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   	 String loginMode=null;
+   	System.out.println("Welcome to Galaxy");
+   	boolean login= false;
+   		while (!login)
+   	{
+	    	System.out.println("type login to login or  Register to Register");
+	        try {
+	          loginMode = br.readLine();
+	        } catch (IOException ioe) {
+	           System.out.println("IO error trying to read your name!");
+	           System.exit(1);
+	        }
+	        if (loginMode.equals("login"))
+	        		Login();
+	        else
+	        	Register();
+   	}
+        System.out.println("Thanks for the name, " + userName);
         ShellFactory.createConsoleShell("galaxy", "", new HelloWorld())
             .commandLoop(); // and three.
     }
+	
 }
