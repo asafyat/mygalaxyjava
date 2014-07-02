7e881299574145a5c904998a14325e47d9ca1fa6
Your Name <eladyat@gmail.com>
2014-07-02 23:40:17 +0300

    add planet class

---
+++ SimpleShell/src/asg/cliche/sample/models/planet.java
@@ -0,0 +1,33 @@
+package asg.cliche.sample.models;
+public class planet {
+	String idPlayer;
+	int IdSolarSystem;
+	String name;
+	int level;
+	public String getIdPlayer() {
+		return idPlayer;
+	}
+	public void setIdPlayer(String idPlayer) {
+		this.idPlayer = idPlayer;
+	}
+	public int getIdSolarSystem() {
+		return IdSolarSystem;
+	}
+	public void setIdSolarSystem(int idSolarSystem) {
+		IdSolarSystem = idSolarSystem;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	
+}
