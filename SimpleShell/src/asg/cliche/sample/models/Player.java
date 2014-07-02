9112f288471701d21900ed8bcb1d713a477e3f1e
Your Name <eladyat@gmail.com>
2014-07-02 23:37:27 +0300

    add SolarSystem class

---
+++ SimpleShell/src/asg/cliche/sample/models/Player.java
@@ -0,0 +1,36 @@
+package asg.cliche.sample.models;
+
+public class Player {
+		String id;
+		String name;
+		String password;
+		
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public int getLevel() {
+			return level;
+		}
+		public void setLevel(int level) {
+			this.level = level;
+		}
+		int level;
+		
+		
+
+}
---
+++ SimpleShell/src/asg/cliche/sample/models/SolarSystem.java
@@ -0,0 +1,34 @@
+package asg.cliche.sample.models;
+
+public class SolarSystem {
+	String idPlayer;
+	String name;
+	int id;
+	int level;
+	public String getIdPlayer() {
+		return idPlayer;
+	}
+	public void setIdPlayer(String idPlayer) {
+		this.idPlayer = idPlayer;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	
+}
