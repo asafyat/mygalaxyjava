2926c9f2acbba0ab9f3e24974a0a2bb032180594
Your Name <eladyat@gmail.com>
2014-07-11 16:43:18 +0300

    add hibernate and

---
+++ SimpleShell/src/asg/cliche/sample/models/Resources.java
@@ -0,0 +1,22 @@
+package asg.cliche.sample.models;
+
+public class Resources {
+String idPlayer;
+int IdSolarSystem;
+int hydrogen;
+int helium;
+int lithium;
+int iron;
+int deuteride;
+
+/*hydrogen+hydrogen=deuteride    *2
+ * hydrogen+deuteride=he3    *2
+ * h3+h3=beryllium
+ * beryllium---> helium+hydrogen+hydrogen
+
+
+
+ */
+}
+
+
