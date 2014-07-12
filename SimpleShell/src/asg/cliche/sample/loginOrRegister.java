80eb0aaac6ae999cd1c7cbdccc015a03c5f68313
Your Name <eladyat@gmail.com>
2014-07-12 12:30:53 +0300

    add Register or login

--- SimpleShell/src/asg/cliche/sample/loginOrRegister.java
+++ SimpleShell/src/asg/cliche/sample/loginOrRegister.java
@@ -11,7 +11,7 @@ import javax.swing.text.StyledEditorKit.BoldAction;
 
 public class loginOrRegister {
    private static String userName = null;
-
+   	
     @Command//the start command
     public String hello() {
         return "welcome to Galaxy Game";
@@ -28,54 +28,62 @@ public class loginOrRegister {
     }
   private static  void Register()
 	{
-		
+	  String Id;
+	  String name;
+	  String password;
+	  System.out.println("enter your mail");
+	  Id=InputText();
+	  System.out.println("enter your name");
+	  name=InputText();
+	  System.out.println("enter your password");
+	  password=InputText();
 	}
 	private static  boolean Login() 
-	{
-		boolean ret=false;
+	{	
+		String Id;
+		String password;
+
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		System.out.print("username:");
-
-
-        try {
-           userName = br.readLine();
-        } catch (IOException ioe) {
-           System.out.println("IO error trying to read your name!");
-           System.exit(1);
-        }
-        
-        
-
-        
-   	System.out.println("password");
-   
+		boolean ret=false;
+		System.out.println("enter your mail");
+       Id=InputText();
+		System.out.println("enter your password");
+				password=InputText();
    	
    	
    	
    	return ret;
 	}
+	private static  String InputText()
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input= null;
+		try {
+			input = br.readLine();
+      } catch (IOException ioe) {
+         System.out.println("IO error trying to read your name!");
+         System.exit(1);
+         
+      }
+		return input;
+  }
     public static void main(String[] args) throws IOException
     {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
    	 String loginMode=null;
    	System.out.println("Welcome to Galaxy");
+   	System.out.println("write Login to Login or Register to Register");
    	boolean login= false;
-   		while (!login)
+   	while (!login)
    	{
-	    	System.out.println("type login to login or  Register to Register");
-	        try {
-	          loginMode = br.readLine();
-	        } catch (IOException ioe) {
-	           System.out.println("IO error trying to read your name!");
-	           System.exit(1);
-	        }
+   			loginMode=InputText();
+	        
 	        if (loginMode.equals("login"))
 	        		Login();
 	        else
 	        	Register();
    	}
+    
         System.out.println("Thanks for the name, " + userName);
         ShellFactory.createConsoleShell("galaxy", "", new loginOrRegister())
             .commandLoop(); // and three.
